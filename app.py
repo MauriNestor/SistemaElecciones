de2fcca886e57cc3c19ce6fb61da04b649e6c18c
AndyOrtz <andy.ortiz.f21@gmail.com>
2024-02-02 09:10:18 -0400

    mejorado de interfaz y accion de contar

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, Integer, String, func
+
 # from elector import Elector
 # from candidato import Candidato
 # from comite_electoral import Comite_electoral
@@ -12,151 +14,120 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = 'cochabamba'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://Slend:@localhost/nombre_de_la_base_de_datos'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-#db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     '{SGBD}://{usuario}:{clave}@{servidor}/{database}'.format(
         SGBD = 'mysql+mysqlconnector',
-        usuario = 'Slend',
-        clave = 'Lx7#Anz0',
-        #clave = 'admin',
+        usuario = 'MauriApaza845',
+        clave = 'Mauricio67532900.',
         servidor = 'MauriApaza845.mysql.pythonanywhere-services.com',
         database = 'MauriApaza845$sistemaEleccionesDB'
     )
 db = SQLAlchemy(app)
 
+class Elector(db.Model):
+    ci_elector = db.Column(db.String(8), primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+    apellido_paterno = db.Column(db.String(80), nullable=False)
+    apellido_materno = db.Column(db.String(80), nullable=False)
+    fecha_nacimiento = db.Column(db.Date, nullable=False)
+    estado = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
-# class LoginForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Login')
+
+class Candidato(db.Model):
+    id_candidato = db.Column(db.String(8), primary_key=True)
+    nombre_candidato = db.Column(db.String(50), nullable=False)
+    nombre_partido = db.Column(db.String(20), nullable=False)
+    foto_candidato = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return '<Name %r>' % self.name
+    def contar_votos(self):
+        return db.session.query(func.count(Voto.id_voto)).filter_by(id_candidato=self.id_candidato).scalar()
+
+class Voto(db.Model):
+    id_voto = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    ci_elector = db.Column(db.String(8), db.ForeignKey('elector.ci_elector'), nullable=False)
+    id_candidato = db.Column(db.String(8), db.ForeignKey('candidato.id_candidato'), nullable=False)
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+class Comite(db.Model):
+    __tablename__ = 'comite_electoral'
+    id_comite = db.Column(db.Integer,primary_key = True,nullable=False)
+    ci_comite = db.Column(db.String(10),nullable=False)
+    contrasena = db.Column(db.String(25),nullable=False)
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 @app.route('/')
 def index():
     return render_template('loginUsuario.html')
-# @app.route('/verificar', methods=['POST'])
-# def verificar():
-#     id_elector = request.form['input-carnet']
-#     fn_elector_str = request.form['input-fn']
-
-#     if not id_elector or not fn_elector_str:
-#         flash('Por favor, complete ambos campos.', 'error')
-#         return render_template('loginUsuario.html')
-
-#     fn_elector = datetime.strptime(fn_elector_str, '%Y-%m-%d').date()
-
-#     indice_elector = -1
-
-#     for elector in electores:
-#         ci = str(elector.get_ci())
-#         nombre = str(elector.get_nombre())  # nombre
-#         habilitado =  elector.habilitado # habilitado
-#         apellido1 = elector.apellidoPaterno
-#         apellido2 = elector.apellidoMaterno
-#         fechaNacimiento = datetime.strptime(elector.fnacmiento, '%Y-%m-%d').date()
-
-#         if ci == id_elector and fechaNacimiento == fn_elector:
-#             return render_template('perfil.html', nombre=nombre, apellidoPaterno = apellido1, apellidoMaterno = apellido2, ci=id_elector,fechaNacimiento=fn_elector,habilitado=habilitado,indice_elector=indice_elector)
-
-#     flash('CI y/o fecha de nacimiento incorrecta, por favor vuelve a ingresar los datos.', 'error')
-#     return render_template('loginUsuario.html')
 
 
+@app.route('/verificar', methods=['POST'])
+def verificar():
+    elector = Elector.query.filter_by(ci_elector=request.form['input-carnet']).first()
 
-
-
-
-
-# def obtener_lista_de_electores():
-#     return electores
-
-
-# @app.route('/irVotar', methods=['POST'])
-# def irVotar():
-#     electores = obtener_lista_de_electores()
-
-#     return render_template('votar.html', candidatos=candidatos, electores=electores)
-
-
-
-
-# @app.route('/realizar_votacion', methods=['POST'])
-# def realizar_votacion():
-#     elector_ci = request.form.get('elector_ci')
-#     candidato_ci = request.form.get('candidato_ci')
-
-#     for elector in electores:
-#         if str(elector.get_ci())==elector_ci:
-#             elector.habilitado.setter('no habilitado')
-#             for candidato in candidatos:
-#                 if str(candidato.get_ci()) == candidato_ci:
-#                     candidato.aumentar_votos()
-#                     flash('¡Tu voto ha sido registrado!', 'success')
-#                     return render_template('loginUsuario.html')
-
-
-#     return render_template('verResultados.html', candidatos=candidatos)
+    if elector:
+        pass_elec = elector.fecha_nacimiento.strftime('%Y-%m-%d')
+        if request.form['input-fn'] == pass_elec:
+            session['elector_logueado'] = elector.ci_elector
+            flash(elector.nombre + ' logueado con éxito!')
+            return render_template('perfil.html', nombre=elector.nombre, apellidoPaterno = elector.apellido_paterno, apellidoMaterno = elector.apellido_materno, ci=elector.ci_elector, fechaNacimiento= elector.fecha_nacimiento, habilitado= elector.estado)
+        else:
+            flash('CI y/o fecha de nacimiento incorrecta, por favor vuelve a ingresar los datos.', 'error')
+            return render_template('loginUsuario.html')
+    else:
+        flash('CI y/o fecha de nacimiento incorrecta, por favor vuelve a ingresar los datos.', 'error')
+        return render_template('loginUsuario.html')
 
 
 @app.route('/comite')
 def index2():
         return render_template('loginComite.html')
 
+
 @app.route('/verificar_comite', methods=['POST'])
 def verificar_comite():
-    return render_template('verResultados.html')
-
-# @app.route('/verificar_comite', methods=['POST'])
-# def verificar_comite():
-#     id_comite = request.form['input-ci']
-#     pass_comite = request.form['input-contra']
-
-#     registrado = False
-
-#     if not id_comite or not pass_comite:
-#         flash('Por favor, complete ambos campos.', 'error')
-#         return render_template('loginComite.html')
-
-#     indice_elector = -1
-
-#     for comite in comites:
-#         ci = str(comite.get_ci())
-#         contra = str(comite.get_contrasena)
-
-#         if ci == id_comite and contra == pass_comite:
-#             registrado = True
-#             return render_template('verResultados.html', candidatos=candidatos)
-
-#     if not registrado:
-#         flash('CI y/o Contraseña incorrecta, por favor vuelve a ingresar los datos.', 'error')
-
-#     return render_template('loginComite.html', candidatos=candidatos)
+    comite = Comite.query.filter_by(ci_comite=request.form['input-ci']).first()
+    if comite:
+        if request.form['input-contra'] == comite.contrasena:
+            session['comite_logueado'] = comite.ci_comite
+            flash('bienvenido')
+            candidatos = Candidato.query.all()
+            return render_template('verResultados.html',candidatos = candidatos)
+        else:
+            flash('CI y/o contraseña incorrecta, por favor vuelve a ingresar los datos.', 'error')
+            return render_template('loginComite.html')
 
 
 
-# voto1 = Voto(candidato1)
-# voto2 = Voto(candidato1)
-# voto3 = Voto(candidato2)
-# voto4 = Voto(candidato3)
-# voto5 = Voto(candidato1)
-# voto6 = Voto(candidato2)
+@app.route('/irVotar', methods=['POST'])
+def irVotar():
 
-# votos =[voto1,voto2,voto3,voto4,voto5, voto6]  # lista de votos
-
-# @app.route('/ver_resultado', methods=['GET'])
-# def ver_res():
-#     resultados = calcular_resultados()
-#     return render_template('verResultados.html',resultados=resultados)
+    candidatos = Candidato.query.all()
+    return render_template('votar.html', candidatos=candidatos)
 
 
-# def calcular_resultados():
-#     resultados = {}
-#     for voto in votos:
-#         candidato = voto.get_candidato.get_nombre()  # Acceder al atributo sin paréntesis
-#         partido = voto.get_candidato.get_partido
-#         if candidato not in resultados:
-#             resultados[candidato,partido] = 0
-#         resultados[candidato,partido] += 1
-#     return resultados
+
+
+@app.route('/realizar_votacion', methods=['POST'])
+def realizar_votacion():
+    ci_elector = session.get('elector_logueado')
+    elector_logeado = Elector.query.get(ci_elector)
+    candidatos = Candidato.query.all()
+
+    if elector_logeado:
+        id_candidato = request.form.get('candidato_ci')
+        if id_candidato:
+            voto = Voto(ci_elector=ci_elector, id_candidato=id_candidato)
+            db.session.add(voto)
+            elector_logeado.estado = 'desahibilitado'
+            db.session.commit()
+            flash('Votación realizada con éxito.')
+            return render_template('loginUsuario.html')
+        else:
+            flash('Por favor, selecciona un candidato antes de votar.', 'error')
+            return render_template('votar.html', candidatos=candidatos)
+    return render_template('votar.html', candidatos=candidatos)
